c91157fd6d85f7de9e0149ae26d878a7b9840271
Nataliia Kotelnitska <nkotelnicka@gmail.com>
2022-05-13 12:19:26 +0300

    update js files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Section from './Section/Section';
 import Statistics from './Statistics/Statistics';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+
 class App extends Component {
   state = {
     good: 0,
@@ -23,58 +24,28 @@ class App extends Component {
     `${Math.round((this.state.good / total) * 100)}%`;
 
   render() {
-    // const { good, neutral, bad } = this.state;
+    const { good, neutral, bad } = this.state;
     const total = this.countTotalFeedback(this.state);
-    let positivePercentage = this.countPositiveFeedbackPercentage(total);
-    // const statisticsData = { ...this.state, total, positivePercentage };
+    const positivePercentage = this.countPositiveFeedbackPercentage(total);
 
     return (
-      <div
-        style={{
-          height: '500px',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 30,
-          color: '#010101',
-        }}
-      >
+      <>
         <Section title="Please leave feedback">
           <FeedbackOptions
             options={this.state}
             onButtonClick={this.onLeaveFeedback}
           />
           <Statistics
-            good={this.state.good}
-            neutral={this.state.neutral}
-            bad={this.state.bad}
+            good={good}
+            neutral={neutral}
+            bad={bad}
             total={total}
             positivePercentage={positivePercentage}
           />
         </Section>
-      </div>
+      </>
     );
   }
 }
 
 export default App;
-
-// export const App = () => {
-//   return (
-//     <div
-//       style={{
-//         height: '500px',
-//         // display: 'flex',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         fontSize: 30,
-//         color: '#010101',
-//       }}
-//     >
-//       {/* React homework template */}
-//       <Section title="Please leave feedback">
-//         <FeedbackOptions />
-//         <Statistics />
-//       </Section>
-//     </div>
-//   );
-// };
